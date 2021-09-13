d242a6a02a0c7bb0673698fa20e8a0dcb223fb90
BVanBuren <bvanburenwx@gmail.com>
2021-09-13 10:56:36 -0400

    added all the folder/files that are needed to get started, updated the schema.sql fille and the connection.js file

---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+-- If this database with the same name already exists it will drop the database
+DROP DATABASE IF EXISTS techBlog_db;
+-- Creates the database with the selected name
+CREATE DATABASE techBlog_db;
